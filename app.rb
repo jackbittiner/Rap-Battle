b810bed52a7f6f02c302809e91e6f113ad68b34c
Jack Bittiner <jackbittiner@hotmail.com>
2017-03-28 15:46:15 +0100

    Submit names function added

--- app.rb
+++ app.rb
@@ -2,10 +2,17 @@ require 'sinatra/base'
 
 class RapBattle < Sinatra::Base
 
-  get '/' do
-    "Testing infrastructure working!"
+  run! if app_file == $0
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
   end
 
-  run! if app_file == $0
+  get '/' do
+    erb(:index)
+  end
+
 
 end
