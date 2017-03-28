1d0db7ecf4d4caeeb5be9203def98e00658173c9
Jack Bittiner <jackbittiner@hotmail.com>
2017-03-28 16:45:37 +0100

    Adding hitpoints for player 2

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'Testing name input' do
+  scenario 'Can input name and submit to see names on screen' do
+    visit('/')
+    fill_in :player_1_name, with: "Coolio"
+    fill_in :player_2_name, with: "Vanilla Ice"
+    click_button "Submit"
+    expect(page).to have_content("Vanilla Ice: 100HP")
+  end
+end
